78725dff6e4d8dbe895e61b7e3d5c9ae0cad2220
Jamie Humphries <jamie.humphries@softwire.com>
2021-08-31 18:27:45 +0100

    Crude pass at move counts.

--- src/build.js
+++ src/build.js
@@ -7,6 +7,8 @@ const rimraf = require("rimraf");
 
 const PVPOKE_FORMAT_SELECT =
   "https://raw.githubusercontent.com/pvpoke/pvpoke/master/src/modules/formatselect.php";
+const GAME_MASTER =
+  "https://raw.githubusercontent.com/pvpoke/pvpoke/master/src/data/gamemaster.json";
 
 const root = "docs";
 rimraf.sync(root);
@@ -19,14 +21,25 @@ fs.writeFileSync(join(root, ".nojekyll"), "");
 fs.writeFileSync(join(root, "CNAME"), "www.pokemoves.com");
 
 async function build() {
+  const gameMaster = await getGameMaster();
+  const moves = parseMoves(gameMaster);
   const formats = await getFormats();
   for (const format of formats) {
     const { cup, cpLimit } = format;
-    const html = await getHtml(format);
+    const html = await getHtml(format, moves);
     const dir = join(root, cup, cpLimit);
     fs.mkdirSync(dir, { recursive: true });
     fs.writeFileSync(join(dir, "index.html"), html);
   }
+}
+
+async function getGameMaster() {
+  return await axios.get(GAME_MASTER).then((res) => res.data);
+}
+
+function parseMoves(gameMaster) {
+  const entries = gameMaster.moves.map((move) => [move.moveId, move]);
+  return Object.fromEntries(entries);
 }
 
 async function getFormats() {
@@ -42,14 +55,21 @@ async function getFormats() {
     .filter((format) => format.cup !== "custom");
 }
 
-async function getHtml({ cup, cpLimit, title }) {
-  const list = await getList(cup, cpLimit);
+async function getHtml(format, moves) {
+  const { cup, cpLimit, title } = format;
+  const list = await getList(cup, cpLimit, moves);
   return nunjucks.render("format.njk", { title, list });
 }
 
-async function getList(cup, cpLimit) {
+async function getList(cup, cpLimit, moves) {
   const url = `https://raw.githubusercontent.com/pvpoke/pvpoke/master/src/data/rankings/${cup}/overall/rankings-${cpLimit}.json`;
-  return await axios.get(url).then((res) => res.data);
+  const list = await axios.get(url).then((res) => res.data);
+  const getMove = ({ moveId }) => moves[moveId];
+  for (const pokemon of list) {
+    pokemon.moves.fastMoves = pokemon.moves.fastMoves.map(getMove);
+    pokemon.moves.chargedMoves = pokemon.moves.chargedMoves.map(getMove);
+  }
+  return list;
 }
 
 build();

863b46f70fb01c41844c11f881d91bbf167cab58
Jamie Humphries <jamie.humphries@outlook.com>
2021-09-17 04:38:21 +0100

    Fix update scripts.

--- src/data/update.js
+++ src/data/update.js
@@ -6,7 +6,7 @@ const GAME_MASTER_ROOT =
   "https://raw.githubusercontent.com/PokeMiners/game_masters/master/latest/";
 
 ["latest.json", "timestamp.txt"].forEach((filename) => {
-  const path = join(__dirname, filename);
+  const path = join(__dirname, "pokeminers", filename);
   const file = fs.createWriteStream(path);
   https.get(GAME_MASTER_ROOT + filename, (res) => res.pipe(file));
 });

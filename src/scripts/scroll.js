dda14be1df549cc6a4d5afaf39bd0b44b3dab574
Jamie Humphries <jamie.humphries@outlook.com>
2021-09-17 08:22:50 +0100

    Add arrow key navigation.

---
+++ src/scripts/scroll.js
@@ -0,0 +1,41 @@
+(function () {
+  const THRESHOLD_PIXELS = 5;
+
+  const elements = document.querySelectorAll("[data-name]");
+  const fromTop = [...elements];
+  const fromBottom = [...elements].reverse();
+
+  window.addEventListener("keydown", function (e) {
+    if (e.ctrlKey || e.metaKey || e.altKey || e.shiftKey) {
+      return;
+    }
+    if (e.key === "ArrowDown") {
+      e.preventDefault();
+      next();
+    }
+    if (e.key === "ArrowUp") {
+      e.preventDefault();
+      previous();
+    }
+  });
+
+  function next() {
+    for (const el of fromTop) {
+      const { top } = el.getBoundingClientRect();
+      if (top > THRESHOLD_PIXELS) {
+        window.scrollBy(0, top);
+        break;
+      }
+    }
+  }
+
+  function previous() {
+    for (const el of fromBottom) {
+      const { top } = el.getBoundingClientRect();
+      if (top < -THRESHOLD_PIXELS) {
+        window.scrollBy(0, top);
+        break;
+      }
+    }
+  }
+})();

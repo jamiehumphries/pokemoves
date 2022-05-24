f94335f2e0e6ea525716d6217cdb1dfcfc0de6db
Jamie Humphries <jamie.humphries@softwire.com>
2022-05-24 15:11:39 +0100

    Remove PokeMiners files from repository.

--- src/data/update.js
+++ src/data/update.js
@@ -5,8 +5,13 @@ const { join } = require("path");
 const GAME_MASTER_ROOT =
   "https://raw.githubusercontent.com/PokeMiners/game_masters/master/latest/";
 
+const localDir = join(__dirname, "pokeminers");
+if (!fs.existsSync(localDir)) {
+  fs.mkdirSync(localDir);
+}
+
 ["latest.json", "timestamp.txt"].forEach((filename) => {
-  const path = join(__dirname, "pokeminers", filename);
+  const path = join(localDir, filename);
   const file = fs.createWriteStream(path);
   https.get(GAME_MASTER_ROOT + filename, (res) => res.pipe(file));
 });
